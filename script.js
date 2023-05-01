989439c9db15c2f6059e8eb9ca2a234726b1642b
Alexandr Leshko <sasha.leshko99@gmail.com>
2023-05-01 23:26:01 +0300

    fix: JS logic (mon 01.05.2023 23:25)

--- script.js
+++ script.js
@@ -3,70 +3,70 @@ wrapper.classList.add('wrapper')
 wrapper.innerHTML =
   '<textarea class="textarea"></textarea>' +
   '<div class="keyboard">' +
-    '<div data-code="Backquote" class="key doubled dark"><span>`</span><span class="upper">~</span></div>' +
-    '<div data-code="Digit1" class="key doubled"><span>1</span><span class="upper">!</span></div>' +
-    '<div data-code="Digit2" class="key doubled"><span>2</span><span class="upper">@</span></div>' +
-    '<div data-code="Digit3" class="key doubled"><span>3</span><span class="upper">#</span></div>' +
-    '<div data-code="Digit4" class="key doubled"><span>4</span><span class="upper">$</span></div>' +
-    '<div data-code="Digit5" class="key doubled"><span>5</span><span class="upper">%</span></div>' +
-    '<div data-code="Digit6" class="key doubled"><span>6</span><span class="upper">^</span></div>' +
-    '<div data-code="Digit7" class="key doubled"><span>7</span><span class="upper">&</span></div>' +
-    '<div data-code="Digit8" class="key doubled"><span>8</span><span class="upper">*</span></div>' +
-    '<div data-code="Digit9" class="key doubled"><span>9</span><span class="upper">(</span></div>' +
-    '<div data-code="Digit0" class="key doubled"><span>0</span><span class="upper">)</span></div>' +
-    '<div data-code="Minus" class="key doubled"><span>-</span><span class="upper">_</span></div>' +
-    '<div data-code="Equal" class="key doubled"><span>=</span><span class="upper">+</span></div>' +
-    '<div data-code="Backspace" class="key wide dark">Backspace</div>' +
-    '<div data-code="Tab" class="key wide dark">Tab ⭾</div>' +
-    '<div data-code="KeyQ" class="key letter"><span class="show">Q</span><span>Й</span></div>' +
-    '<div data-code="KeyW" class="key letter"><span class="show">W</span><span>Ц</span></div>' +
-    '<div data-code="KeyE" class="key letter"><span class="show">E</span><span>У</span></div>' +
-    '<div data-code="KeyR" class="key letter"><span class="show">R</span><span>К</span></div>' +
-    '<div data-code="KeyT" class="key letter"><span class="show">T</span><span>Е</span></div>' +
-    '<div data-code="KeyY" class="key letter"><span class="show">Y</span><span>Н</span></div>' +
-    '<div data-code="KeyU" class="key letter"><span class="show">U</span><span>Г</span></div>' +
-    '<div data-code="KeyI" class="key letter"><span class="show">I</span><span>Ш</span></div>' +
-    '<div data-code="KeyO" class="key letter"><span class="show">O</span><span>Щ</span></div>' +
-    '<div data-code="KeyP" class="key letter"><span class="show">P</span><span>З</span></div>' +
-    '<div data-code="BracketLeft" class="key multikey"><span class="show"><span>[</span><span class="upper">{</span></span><span><span>Х</span></span></div>' +
-    '<div data-code="BracketRight" class="key multikey"><span class="show"><span>[</span><span class="upper">}</span></span><span><span>Ъ</span></span></div>' +
-    '<div data-code="Delete" class="key dark">Del</div>' +
-    '<div data-code="CapsLock" class="key wide dark">Caps Lock</div>' +
-    '<div data-code="KeyA" class="key letter"><span class="show">A</span><span>Ф</span></div>' +
-    '<div data-code="KeyS" class="key letter"><span class="show">S</span><span>Ы</span></div>' +
-    '<div data-code="KeyD" class="key letter"><span class="show">D</span><span>В</span></div>' +
-    '<div data-code="KeyF" class="key letter"><span class="show">F</span><span>А</span></div>' +
-    '<div data-code="KeyG" class="key letter"><span class="show">G</span><span>П</span></div>' +
-    '<div data-code="KeyH" class="key letter"><span class="show">H</span><span>Р</span></div>' +
-    '<div data-code="KeyJ" class="key letter"><span class="show">J</span><span>О</span></div>' +
-    '<div data-code="KeyK" class="key letter"><span class="show">K</span><span>Л</span></div>' +
-    '<div data-code="KeyL" class="key letter"><span class="show">L</span><span>Д</span></div>' +
-    '<div data-code="Semicolon" class="key multikey"><span class="show"><span>;</span><span class="upper">:</span></span><span><span>Ж</span></span></div>' +
-    '<div data-code="Quote" class="key multikey"><span class="show"><span>\'</span><span class="upper">"</span></span><span><span>Э</span></span></div>' +
-    '<div data-code="Enter" class="key wide dark">Enter</div>' +
-    '<div data-code="ShiftLeft" class="key wide dark">Shift ⇧</div>' +
-    '<div data-code="Backslash" class="key multikey"><span class="show"><span>\\</span><span class="upper">|</span></span><span><span>\\</span><span class="upper">/</span></span></div>' +
-    '<div data-code="KeyZ" class="key letter"><span class="show">Z</span><span>Я</span></div>' +
-    '<div data-code="KeyX" class="key letter"><span class="show">X</span><span>Ч</span></div>' +
-    '<div data-code="KeyC" class="key letter"><span class="show">C</span><span>С</span></div>' +
-    '<div data-code="KeyV" class="key letter"><span class="show">V</span><span>М</span></div>' +
-    '<div data-code="KeyB" class="key letter"><span class="show">B</span><span>И</span></div>' +
-    '<div data-code="KeyN" class="key letter"><span class="show">N</span><span>Т</span></div>' +
-    '<div data-code="KeyM" class="key letter"><span class="show">M</span><span>Ь</span></div>' +
-    '<div data-code="Comma" class="key multikey"><span class="show"><span>,</span><span class="upper">&lt;</span></span><span><span>Б</span></span></div>' +
-    '<div data-code="Period" class="key multikey"><span class="show"><span>.</span><span class="upper">&gt;</span></span><span><span>Ю</span></span></div>' +
-    '<div data-code="Slash" class="key multikey"><span class="show"><span>/</span><span class="upper">?</span></span><span><span>.</span><span class="upper">,</span></span></div>' +
-    '<div data-code="ArrowUp" class="key symbol dark">▲</div>' +
-    '<div data-code="ShiftRight" class="key dark">⇧</div>' +
-    '<div data-code="ControlLeft" class="key dark">Ctrl</div>' +
-    '<div data-code="MetaLeft" class="key dark">⊞</div>' +
-    '<div data-code="AltLeft" class="key dark">Alt</div>' +
-    '<div data-code="Space" class="key symbol ultra-wide"> </div>' +
-    '<div data-code="AltRight" class="key dark">Alt</div>' +
-    '<div data-code="ControlRight" class="key dark">Ctrl</div>' +
-    '<div data-code="ArrowLeft" class="key symbol dark">◀</div>' +
-    '<div data-code="ArrowDown" class="key symbol dark">▼</div>' +
-    '<div data-code="ArrowRight" class="key symbol dark">▶</div>' +
+  '<div data-code="Backquote" class="key doubled dark"><span>`</span><span class="upper">~</span></div>' +
+  '<div data-code="Digit1" class="key doubled"><span>1</span><span class="upper">!</span></div>' +
+  '<div data-code="Digit2" class="key doubled"><span>2</span><span class="upper">@</span></div>' +
+  '<div data-code="Digit3" class="key doubled"><span>3</span><span class="upper">#</span></div>' +
+  '<div data-code="Digit4" class="key doubled"><span>4</span><span class="upper">$</span></div>' +
+  '<div data-code="Digit5" class="key doubled"><span>5</span><span class="upper">%</span></div>' +
+  '<div data-code="Digit6" class="key doubled"><span>6</span><span class="upper">^</span></div>' +
+  '<div data-code="Digit7" class="key doubled"><span>7</span><span class="upper">&</span></div>' +
+  '<div data-code="Digit8" class="key doubled"><span>8</span><span class="upper">*</span></div>' +
+  '<div data-code="Digit9" class="key doubled"><span>9</span><span class="upper">(</span></div>' +
+  '<div data-code="Digit0" class="key doubled"><span>0</span><span class="upper">)</span></div>' +
+  '<div data-code="Minus" class="key doubled"><span>-</span><span class="upper">_</span></div>' +
+  '<div data-code="Equal" class="key doubled"><span>=</span><span class="upper">+</span></div>' +
+  '<div data-code="Backspace" class="key wide dark">Backspace</div>' +
+  '<div data-code="Tab" class="key wide dark">Tab ⭾</div>' +
+  '<div data-code="KeyQ" class="key letter"><span class="show">Q</span><span>Й</span></div>' +
+  '<div data-code="KeyW" class="key letter"><span class="show">W</span><span>Ц</span></div>' +
+  '<div data-code="KeyE" class="key letter"><span class="show">E</span><span>У</span></div>' +
+  '<div data-code="KeyR" class="key letter"><span class="show">R</span><span>К</span></div>' +
+  '<div data-code="KeyT" class="key letter"><span class="show">T</span><span>Е</span></div>' +
+  '<div data-code="KeyY" class="key letter"><span class="show">Y</span><span>Н</span></div>' +
+  '<div data-code="KeyU" class="key letter"><span class="show">U</span><span>Г</span></div>' +
+  '<div data-code="KeyI" class="key letter"><span class="show">I</span><span>Ш</span></div>' +
+  '<div data-code="KeyO" class="key letter"><span class="show">O</span><span>Щ</span></div>' +
+  '<div data-code="KeyP" class="key letter"><span class="show">P</span><span>З</span></div>' +
+  '<div data-code="BracketLeft" class="key multikey"><span class="show"><span>[</span><span class="upper">{</span></span><span><span>Х</span></span></div>' +
+  '<div data-code="BracketRight" class="key multikey"><span class="show"><span>[</span><span class="upper">}</span></span><span><span>Ъ</span></span></div>' +
+  '<div data-code="Delete" class="key dark">Del</div>' +
+  '<div data-code="CapsLock" class="key wide dark">Caps Lock</div>' +
+  '<div data-code="KeyA" class="key letter"><span class="show">A</span><span>Ф</span></div>' +
+  '<div data-code="KeyS" class="key letter"><span class="show">S</span><span>Ы</span></div>' +
+  '<div data-code="KeyD" class="key letter"><span class="show">D</span><span>В</span></div>' +
+  '<div data-code="KeyF" class="key letter"><span class="show">F</span><span>А</span></div>' +
+  '<div data-code="KeyG" class="key letter"><span class="show">G</span><span>П</span></div>' +
+  '<div data-code="KeyH" class="key letter"><span class="show">H</span><span>Р</span></div>' +
+  '<div data-code="KeyJ" class="key letter"><span class="show">J</span><span>О</span></div>' +
+  '<div data-code="KeyK" class="key letter"><span class="show">K</span><span>Л</span></div>' +
+  '<div data-code="KeyL" class="key letter"><span class="show">L</span><span>Д</span></div>' +
+  '<div data-code="Semicolon" class="key multikey"><span class="show"><span>;</span><span class="upper">:</span></span><span><span>Ж</span></span></div>' +
+  '<div data-code="Quote" class="key multikey"><span class="show"><span>\'</span><span class="upper">"</span></span><span><span>Э</span></span></div>' +
+  '<div data-code="Enter" class="key wide dark">Enter</div>' +
+  '<div data-code="ShiftLeft" class="key wide dark">Shift ⇧</div>' +
+  '<div data-code="Backslash" class="key multikey"><span class="show"><span>\\</span><span class="upper">|</span></span><span><span>\\</span><span class="upper">/</span></span></div>' +
+  '<div data-code="KeyZ" class="key letter"><span class="show">Z</span><span>Я</span></div>' +
+  '<div data-code="KeyX" class="key letter"><span class="show">X</span><span>Ч</span></div>' +
+  '<div data-code="KeyC" class="key letter"><span class="show">C</span><span>С</span></div>' +
+  '<div data-code="KeyV" class="key letter"><span class="show">V</span><span>М</span></div>' +
+  '<div data-code="KeyB" class="key letter"><span class="show">B</span><span>И</span></div>' +
+  '<div data-code="KeyN" class="key letter"><span class="show">N</span><span>Т</span></div>' +
+  '<div data-code="KeyM" class="key letter"><span class="show">M</span><span>Ь</span></div>' +
+  '<div data-code="Comma" class="key multikey"><span class="show"><span>,</span><span class="upper">&lt;</span></span><span><span>Б</span></span></div>' +
+  '<div data-code="Period" class="key multikey"><span class="show"><span>.</span><span class="upper">&gt;</span></span><span><span>Ю</span></span></div>' +
+  '<div data-code="Slash" class="key multikey"><span class="show"><span>/</span><span class="upper">?</span></span><span><span>.</span><span class="upper">,</span></span></div>' +
+  '<div data-code="ArrowUp" class="key symbol dark">▲</div>' +
+  '<div data-code="ShiftRight" class="key dark">⇧</div>' +
+  '<div data-code="ControlLeft" class="key dark">Ctrl</div>' +
+  '<div data-code="MetaLeft" class="key dark">⊞</div>' +
+  '<div data-code="AltLeft" class="key dark">Alt</div>' +
+  '<div data-code="Space" class="key symbol ultra-wide"> </div>' +
+  '<div data-code="AltRight" class="key dark">Alt</div>' +
+  '<div data-code="ControlRight" class="key dark">Ctrl</div>' +
+  '<div data-code="ArrowLeft" class="key symbol dark">◀</div>' +
+  '<div data-code="ArrowDown" class="key symbol dark">▼</div>' +
+  '<div data-code="ArrowRight" class="key symbol dark">▶</div>' +
   '</div>' +
   '<p class="notation">Keyboard was made on Windows. Use <span>Alt + Shift</span> to switch language. Click <span>Shift</span> to make next letter in upper case.'
 document.body.prepend(wrapper)
@@ -78,7 +78,12 @@ const LETTERS = document.getElementsByClassName('letter')
 const SYMBOLS = document.getElementsByClassName('symbol')
 const MULTIKEYS = document.getElementsByClassName('multikey')
 
-let isRU = 0
+let isRU
+if (document.cookie.slice(5) === '') {
+  isRU = 0
+} else {
+  isRU = document.cookie.slice(5)
+}
 function setLanguage () {
   if (isRU % 2) {
     for (const letter of LETTERS) {
@@ -100,13 +105,9 @@ function setLanguage () {
     }
   }
 }
+setLanguage()
 
-let isCaps
-if (document.cookie.slice(7) === '') {
-  isCaps = 0
-} else {
-  isCaps = document.cookie.slice(7)
-}
+let isCaps = 0
 function toUpperRegister () {
   return (isCaps % 2)
 }
@@ -133,11 +134,11 @@ document.addEventListener('keydown', e => {
   }
   if (e.code === 'CapsLock') {
     isCaps++
-    document.cookie = `isCaps=${isCaps}`
   };
   if (e.altKey || e.code === 'Tab') e.preventDefault()
   if (e.shiftKey && e.altKey) {
     isRU++
+    document.cookie = `isRU=${isRU}`
     setLanguage()
   }
   for (const key of KEYS) {
@@ -215,7 +216,6 @@ document.querySelector('.key[data-code="Enter"]').addEventListener('click', () =
 // CapsLock
 document.querySelector('.key[data-code="CapsLock"]').addEventListener('click', () => {
   isCaps++
-  document.cookie = `isCaps=${isCaps}`
 })
 
 // Shift
